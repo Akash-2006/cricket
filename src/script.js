34debc915a96383e94c231ee6997bb2f7e692dd4
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2025-03-10 10:18:37 +0530

    combined login page and cards generator

--- src/script.js
+++ src/script.js
@@ -1,15 +1,25 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { generateCard } from "./card.js";
-import details from '../json/match1.json' with {type: 'json'};
+import { compare } from "../utilites/auticantaion.js";
+
 const data = Deno.readTextFileSync('html/index.html');
 const router = new Router();
 
 router.get('/', (context) => {
+  const page = Deno.readTextFileSync('html/loginPage.html');
+  context.response.body = page;
+});
+
+router.get('/cards', (context) => {
   const cards = generateCard();
   context.response.body = data.replaceAll('%cards%', cards.join('\n'));
 });
-router.get('/test', (context) => {
-  context.response.body = details;
+
+router.post("/login", async (context) => {
+  const body = await context.request.body.json();
+  const validUser = compare(body);
+  context.response.body = JSON.stringify(validUser);
+  context.response.type = 'json';
 });
 
 router.get("/static/:path*", async (context) => {
---
+++ todo.md
@@ -0,0 +1,8 @@
+Create a json file with one match details
+Create a cricket template
+Display it on the browser
+Create a login page
+Add functionality to it
+Combine both of them
+Add database
+Add styles
